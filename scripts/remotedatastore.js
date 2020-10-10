40315d5cb1b1ac5bd8775f11251ffcbc5e449c61
david810376 <33507197+david810376@users.noreply.github.com>
2020-10-10 14:55:47 -0700

    small fix

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -3,6 +3,8 @@
 var App = window.App || {};
 var $ = window.jQuery;
 
+//book code
+/*
 function RemoteDataStore(url){
     if(!url){
         throw new Error('No remote URL supplied.');
@@ -45,6 +47,34 @@ RemoteDataStore.prototype.remove = function(key){
         type : 'DELETE'
     });
 };
+*/
+
+// prof code
+class RemoteDataStore {
+    constructor(url){
+        console.log('running the DataStore Function');
+        if(!url){
+            throw new Error('No remote URL supplied');
+        };
+        this.serverUrl = url;
+    }
+    ajaxposthelper(type, url, val){
+        $.ajax({ type: type, url: url, contentType: 'application/json', data: JSON.stringify(val),
+        success: function(response){ console.log('function required: ' + JSON.stringify(response));}
+        });
+    }
+    ajaxhelper(type, url, cb) {
+        $.ajax({ type: type, url: url, contentType: 'application/json', 
+        success: function(response){
+            console.log('function returned: ' + JSON.stringify(response));
+            if (cb !== undefined) {cb(response);}
+        }})
+    }
+    add(key,val) { this.ajaxposthelper('POST', this.serverUrl, val);}
+    get(key,cb) { this.ajaxhelper('GET', this.serverUrl + '/' + key,cb);}
+    getAll(cb)  { this.ajaxhelper('GET', this.serverUrl, cb);}
+    remove(key) { this.ajaxhelper('DELETE', this.serverUrl + '/' + key);}
+}
 
 App.RemoteDataStore = RemoteDataStore;
 window.App = App;
